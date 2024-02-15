042fd9ace022b12c96c4a18077dbee0398db7a27
pedro2398 <pedrooo2398@gmail.com>
2024-02-15 18:36:22 -0300

    adding user actions

---
+++ src/actions/userActions.js
@@ -0,0 +1,32 @@
+"use server"
+
+import { revalidatePath } from "next/cache";
+const url = "http://localhost:8080/user";
+
+export async function saveUser(data) {
+    
+    const options = {
+        method: "POST",
+        body: JSON.stringify(Object.fromEntries(data)),
+        headers: {
+            "Content-Type": "application/json"
+        }
+
+    };
+
+    const response = await fetch(url, options);
+
+    if (response.status !== 201) return { error : "error when registering"};
+
+    revalidatePath("/")
+}
+
+export async function getEmail() {
+    const response = await fetch(url,{next: {revalidate: 0}});
+
+    if(response.status != 200) {
+        return {error: "error when find emails"}
+    }
+
+    return await response.json();
+}
