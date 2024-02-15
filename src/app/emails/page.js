f2fa2c145de328cd83e586cc5bbfa34cdf677401
pedro2398 <pedrooo2398@gmail.com>
2024-02-15 18:36:54 -0300

    adding pages

---
+++ src/app/emails/page.js
@@ -0,0 +1,15 @@
+import { getEmail } from "@/actions/userActions";
+import Button from "@/components/button";
+import DisplayUser from "@/components/displayUser";
+
+export default async function Email() {
+    
+    const users = await getEmail();
+    
+    return(
+        <main className="flex flex-col justify-center items-center bg-blue-950 w-96 py-10 rounded-xl space-y-4 mx-auto mt-20">
+            {users.map(usuario => DisplayUser(usuario))}
+                <Button href={ "/" } text={ "home" }/>
+        </main>
+    );
+}
---
+++ src/app/registered/page.js
@@ -0,0 +1,14 @@
+import Button from "@/components/button";
+
+export default function Registered() {
+    return(
+        <main className="flex flex-col justify-center items-center bg-blue-950 w-96 py-10 rounded-xl space-y-4 mx-auto mt-20">
+            <p>User registered successfully!</p>
+            <p>Thank you for responding to our form</p>
+            <div className="space-x-2">
+                <Button href={ "/" } text={ "home" }/>
+                <Button href={ "/emails" } text={"emails"}/>
+            </div>
+        </main>
+    );
+}
