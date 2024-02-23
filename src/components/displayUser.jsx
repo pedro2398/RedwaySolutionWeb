909a20478a8e8c035b9964197aec40e584dbf747
pedro2398 <pedrooo2398@gmail.com>
2024-02-23 20:10:27 -0300

    changing GetUser call location

--- src/components/displayUser.jsx
+++ src/components/displayUser.jsx
@@ -1,12 +1,19 @@
-export default function DisplayUser(user) {
-    return(
-        <div className="flex justify-start px-2 w-[100%]">
-            <p className="mr-2">
-                <span className="text-cyan-300">Name: </span> {user.firstName}
-            </p>
-            <p>
-                <span className="text-cyan-300">Email:</span> {user.email}
-            </p>
-        </div>
+import { getUser } from "@/actions/userActions"
+
+export default async function DisplayUser() {
+   const users = await getUser(); 
+
+    return users.map(user => {
+        return (
+            <div className="flex justify-start px-2 w-[100%]" key={ user.id } >
+                    <p className="mr-2">
+                        <span className="text-cyan-300">Name: </span> {user.firstName} |
+                    </p>
+                    <p>
+                        <span className="text-cyan-300">Email:</span> {user.email}
+                    </p>
+            </div>
+        )
+      }
     )
 }
